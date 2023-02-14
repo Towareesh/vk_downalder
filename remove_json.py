76bbcef84d2c49ca1b974711ffe9b2c6c4340be2
Towareesh <63032856+Towareesh@users.noreply.github.com>
2023-02-14 22:16:02 +0200

    remove
    replace second logic script

--- remove_json.py
+++ remove_json.py
@@ -1,32 +1,69 @@
-import json
 import time
-import os
-import subprocess
+import json
+
+import vk_api
+from vk_api import audio
 
 
-def view_audio(json_data, stop):
-    n = 0
-    for track_data in json_data:
-        n += 1
-        track_name     = '{} - {}.mp3'.format(track_data['artist'], track_data['title'])
-        lisnk_segments = track_data['url']
-        print(f'{n}>> {track_name}')
+class UserLoginVk:
+    def __init__(self, login, password, userid=None, token=None):
+        self.login      = login
+        self.password   = password
+        self.userid     = userid
 
-        # subprocess.call(f"""ffmpeg -i {lisnk_segments} {n}.mp3""", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        subprocess.call(f"""ffmpeg -i {lisnk_segments} {n}.mp3""")
-        os.rename(f'{n}.mp3', track_name)
-
-        if n == stop:
-            break
-        
+        self.vk_session = vk_api.VkApi(login=login,
+                                       password=password,
+                                       auth_handler=self._sms_confirmation,
+                                       token=token)
+        self.vk_session.auth()
+        self.vk = self.vk_session.get_api()
 
 
-with open('my_tracks2.json', 'r', encoding='utf-8') as json_file:
-    data = json.load(json_file)
+    def _sms_confirmation(self):
+        key = input('Enter sms code: ')
+        remember_me = True
+        return key, remember_me
 
 
-start_time = time.time()
+    def _get_audio(self):
+        vk_audio = audio.VkAudio(self.vk_session)
+        return vk_audio
 
-view_audio(data, 2)
-end_time = time.time() - start_time
-print(round(end_time, 2))
+
+    def get_json_audio(self):
+        list_audio = self._get_audio()
+        with open('my_tracks2.json', 'w') as file:
+            json.dump(list_audio.get(owner_id=self.userid), file, indent=2)
+
+
+    def my_albums(self):
+        albums = self._get_audio()
+        with open('my_albums.json', 'w') as file:
+            json.dump(albums.get_albums(owner_id=self.userid), file, indent=2)
+
+
+def main_script(login, password, userid):
+    my_vk = UserLoginVk(login, password, userid)
+    my_vk.my_albums()
+
+def register():
+    userid = input('If have id: ')
+    if not userid:
+        userid = None
+
+    login    = input('Enter login or phone: ')
+    password = input('Enter password: ')
+
+    try:
+        main_script(login, password, userid)
+    except vk_api.exceptions.Captcha as captcha:
+        captcha.sid             # Получение sid
+        captcha.get_url()       # Получить ссылку на изображение капчи
+        captcha.get_image()     # Получить изображение капчи (jpg)
+        main_script(login, password, userid)
+
+if __name__ == '__main__':
+    start_time = time.time()
+    register()
+    end_time = time.time() - start_time
+    print(round(end_time, 2))
